a886ac34c291ec4cc56bec3ef32c6eb761164163
Bogdan Poplauschi <bpoplauschi@gmail.com>
2020-12-25 17:28:02 +0200

    Created FeedItem

---
+++ EssentialFeed/Models/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Bogdan P on 25/12/2020.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
